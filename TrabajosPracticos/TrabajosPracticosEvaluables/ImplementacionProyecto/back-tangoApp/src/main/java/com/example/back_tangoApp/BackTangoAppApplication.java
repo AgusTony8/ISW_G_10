b257669916d49daf534d5348f1b277b6dbd19e7f
Juampi270302 <juampylambertucci2002@gmail.com>
2024-09-08 15:22:09 -0300

    Cosass que no se pushearon

---
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/back-tangoApp/src/main/java/com/example/back_tangoApp/BackTangoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_tangoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTangoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTangoAppApplication.class, args);
+	}
+
+}
