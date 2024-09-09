586bd2c806775803b9915972c7ee2726915c17c1
Juampi270302 <juampylambertucci2002@gmail.com>
2024-09-09 00:28:03 -0300

    Implementado envio de emails a los transportistas de la localidad de retiro

---
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/tango-app-email-server/src/main/java/server/tango_app_email_server/TangoAppEmailServerApplication.java
@@ -0,0 +1,13 @@
+package server.tango_app_email_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangoAppEmailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TangoAppEmailServerApplication.class, args);
+	}
+
+}
---
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/tango-app-email-server/src/test/java/server/tango_app_email_server/TangoAppEmailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.tango_app_email_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangoAppEmailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
