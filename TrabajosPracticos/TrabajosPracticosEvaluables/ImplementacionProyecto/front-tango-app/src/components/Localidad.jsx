49de3246d0a2c755a9da26fce0f1f8e3300f9098
Santiago Ismael Moyano <86203@sistemas.frc.utn.edu.ar>
2024-09-12 17:04:47 -0300

    config.js

--- TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/front-tango-app/src/components/Localidad.jsx
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/front-tango-app/src/components/Localidad.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importar estilos de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React, { useState, useEffect } from "react";
+
+//console.log("mounting Articulos");
+async function ObtenerLocalidades() {
+    let data = await articulosfamiliasService.Buscar();
+    setArticulosFamilias(data);
+  }
+
+
+// cargar al "montar" el componente, solo la primera vez (por la dependencia [])
+useEffect(() => {
+    BuscarArticulosFamilas();
+  }, []);
+
+
 
 const Localidad = () => {
     return (
---
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/front-tango-app/src/config.js
@@ -0,0 +1,10 @@
+const urlServidor = "http://localhost:8080"
+
+const urlLocalidades = "https://apis.datos.gob.ar/georef/api/localidades"
+const urlCalles = "https://apis.datos.gob.ar/georef/api/calles"
+
+export const config = {
+    urlServidor,
+    urlLocalidades,
+    urlCalles
+}
