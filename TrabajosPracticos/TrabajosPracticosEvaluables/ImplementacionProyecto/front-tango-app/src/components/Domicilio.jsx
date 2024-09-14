0128ce0494c0abe126f46e25ec7434984207045a
Santiago Ismael Moyano <86203@sistemas.frc.utn.edu.ar>
2024-09-14 20:02:37 -0300

    Correccion de errores

--- TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/front-tango-app/src/components/Domicilio.jsx
+++ TrabajosPracticos/TrabajosPracticosEvaluables/ImplementacionProyecto/front-tango-app/src/components/Domicilio.jsx
@@ -4,19 +4,7 @@ import InputTexto from './InputTexto.jsx';
 import ListaDesplegable from './ListaDesplegable.jsx';
 import ListaSugerencias from './ListaSugerencias.jsx';
 
-<<<<<<< HEAD
-const Domicilio = ({ onFechaChange }) => {
-  const [provincias, setProvincias] = useState([]);
-  const [inputLocalidad, setInputLocalidad] = useState("");
-  const [inputCalle, setInputCalle] = useState("");
-  const [localidadesOriginales, setLocalidadesOriginales] = useState([]);
-  const [localidadesFiltradas, setLocalidadesFiltradas] = useState([]);
-  const [callesOriginales, setCallesOriginales] = useState([]);
-  const [callesFiltradas, setCallesFiltradas] = useState([]);
-  const [errorLocalidad, setErrorLocalidad] = useState('');
-  const [errorCalle, setErrorCalle] = useState('');
-=======
-const Domicilio = () => {
+const Domicilio = ({onFechaChange}) => {
     const [provincias, setProvincias] = useState([]);
     const [inputLocalidad, setInputLocalidad] = useState("");
     const [inputCalle, setInputCalle] = useState("");
@@ -29,39 +17,30 @@ const Domicilio = () => {
     const [errorCalle, setErrorCalle] = useState('');
     const [errorNumero, setErrorNumero] = useState('');
     const [isSnChecked, setIsSnChecked] = useState(false); // Estado para el checkbox S/N
->>>>>>> 90490544ac5aa540521ff623461fc3513ea06a94
 
   async function BuscarLocalidades(idProvincia) {
     const response = await domiciliosService.LocalidadesPorProvincia(idProvincia);
     setLocalidadesOriginales(response.localidades);
     setLocalidadesFiltradas(response.localidades);
+    setInputLocalidad('')
+    setInputCalle('')
   }
 
-<<<<<<< HEAD
   async function BuscarCalles(idProvincia, idLocalCensal) {
     const response = await domiciliosService.CallePorProvinciaYLocalidad(idProvincia, idLocalCensal);
-    setCallesOriginales(response.calles);
-    setCallesFiltradas(response.calles);
+    setCallesOriginales(response);
+    setCallesFiltradas(response);
+    setInputCalle('')
   }
 
   useEffect(() => {
     async function BuscarProvincias() {
       const response = await domiciliosService.ObtenerProvincias();
       setProvincias(response.provincias);
-=======
-    async function BuscarLocalidades(idProvincia) {
-        const response = await domiciliosService.LocalidadesPorProvincia(idProvincia);
-        setLocalidadesOriginales(response.localidades)
-        setLocalidadesFiltradas(response.localidades)
-        setInputLocalidad('')
-        setInputCalle('')
-
->>>>>>> 90490544ac5aa540521ff623461fc3513ea06a94
     }
     BuscarProvincias();
   }, []);
 
-<<<<<<< HEAD
   const provinciasSelect = provincias.map(provincia => ({
     val: provincia.id,
     text: provincia.nombre
@@ -88,13 +67,6 @@ const Domicilio = () => {
     } else if (value.length === 0) {
       setLocalidadesFiltradas(localidadesOriginales);
       setErrorLocalidad('');
-=======
-    async function BuscarCalles(idProvincia, idLocalCensal) {
-        const response = await domiciliosService.CallePorProvinciaYLocalidad(idProvincia, idLocalCensal)
-        setCallesOriginales(response)
-        setCallesFiltradas(response)
-        setInputCalle('')
->>>>>>> 90490544ac5aa540521ff623461fc3513ea06a94
     }
   };
 
@@ -117,63 +89,8 @@ const Domicilio = () => {
     }
   };
 
-<<<<<<< HEAD
-=======
-    const provinciasSelect = provincias.map( provincia => ({
-        val: provincia.id,
-        text: provincia.nombre
-    }))
 
-    const handleProvinciaChange = (event) => {
-        const idProvincia = event.target.value;
-        BuscarLocalidades(idProvincia)
-      };
-
-    const handleInputChangeLocalidad = (event) => {
-        let value = event.target.value;
-        setInputLocalidad(value);
-        if (localidadesFiltradas && Array.isArray(localidadesFiltradas)) {
-            // Si hay un valor en el input, filtrar las localidades
-            const filtrado = localidadesOriginales.filter(localidad =>
-                localidad.nombre.toLowerCase().startsWith(value.toLowerCase())
-            )
-            setLocalidadesFiltradas(filtrado);
-            if (filtrado.length === 0) {
-                 setErrorLocalidad('No se encontró la localidad.');
-            } else {
-                setErrorLocalidad(''); // Limpiar el mensaje de error si hay resultados
-            }
-        }
-        else if (value.length === 0){
-                // Si el input está vacío, restablecer todas las localidades
-                setLocalidadesFiltradas(localidadesOriginales);
-                setErrorLocalidad(''); // Limpiar el mensaje de error
-            }
-     }
-
-     const handleInputChangeCalle = (event) => {
-        let value = event.target.value;
-        setInputCalle(value);
-        if (callesFiltradas && Array.isArray(callesFiltradas)) {
-            // Si hay un valor en el input, filtrar las localidades
-            const filtrado = callesOriginales.filter(calle =>
-                calle.nombre.toLowerCase().startsWith(value.toLowerCase())
-            )
-            setCallesFiltradas(filtrado);
-            if (filtrado.length === 0) {
-                setErrorCalle('No se encontró la calle.');
-           } else {
-               setErrorCalle(''); // Limpiar el mensaje de error si hay resultados
-           }
-        }
-        else if (value.length === 0){
-                // Si el input está vacío, restablecer todas las localidades
-                setCallesFiltradas(callesOriginales);
-                setErrorCalle(''); // Limpiar el mensaje de error
-            }
-     }
-
-     function handleInputChangeNumero(event) {
+    function handleInputChangeNumero(event) {
         const value = event.target.value;
         
         // Permitir solo números enteros positivos
@@ -196,7 +113,6 @@ const Domicilio = () => {
     };
 
   // Manejar la selección de una sugerencia
->>>>>>> 90490544ac5aa540521ff623461fc3513ea06a94
   const handleSuggestionClickLocalidad = (localidad) => {
     setInputLocalidad(localidad.nombre);
     BuscarCalles(localidad.provincia.id, localidad.localidad_censal.id);
@@ -217,44 +133,9 @@ const Domicilio = () => {
 
   const inputClassLocalidad = errorLocalidad ? 'form-control is-invalid' : 'form-control';
   const feedbackClassLocalidad = errorLocalidad ? 'invalid-feedback' : '';
-<<<<<<< HEAD
+
   const inputClassCalle = errorCalle ? 'form-control is-invalid' : 'form-control';
   const feedbackClassCalle = errorCalle ? 'invalid-feedback' : '';
-
-  return (
-    <>
-      <ListaDesplegable opc={provinciasSelect} titulo="Seleccione una provincia" onChange={handleProvinciaChange} />
-      <div>
-        <InputTexto titulo="Localidad" value={inputLocalidad} label="Localidad" onChange={handleInputChangeLocalidad} valido={inputClassLocalidad} />
-        {errorLocalidad && <div className={feedbackClassLocalidad}>{errorLocalidad}</div>}
-        <ListaSugerencias items={localidadesFiltradas} getKey={(localidad) => localidad.id} getDisplayValue={(localidad) => localidad.nombre} onItemClick={handleSuggestionClickLocalidad} />
-      </div>
-      <h5>Direccion</h5>
-      <div className="input-group mb-3">
-        <input type="text" className={inputClassCalle} placeholder="Calle" value={inputCalle} onChange={handleInputChangeCalle} aria-label="Calle" />
-        {errorCalle && <div className={feedbackClassCalle}>{errorCalle}</div>}
-        <ListaSugerencias items={callesFiltradas} getKey={(calle) => calle.id} getDisplayValue={(calle) => calle.nombre} onItemClick={handleSuggestionClickCalle} />
-        <input type="text" className="form-control" placeholder="Número" aria-label="Número" />
-      </div>
-      <div className="form-floating">
-        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: '100px' }}></textarea>
-        <label htmlFor="floatingTextarea2">Referencia</label>
-      </div>
-      <h5>Fecha</h5>
-      <input type="date" className="form-control" placeholder="Fecha de retiro" aria-label="Fecha Retiro" onChange={handleFechaChange} />
-    </>
-  );
-};
-=======
-
-   // Manejar la selección de una sugerencia
-   const handleSuggestionClickCalle = (calle) => {
-    setInputCalle(calle.nombre); // Actualizar el input con el nombre seleccionado
-    setCallesFiltradas([]); // Ocultar la lista de sugerencias
-    setErrorCalle(''); // Limpiar el mensaje de error
-    };
-    const inputClassCalle = errorCalle ? 'form-control is-invalid' : 'form-control';
-    const feedbackClassCalle = errorCalle ? 'invalid-feedback' : '';
 
     return (
         <>
@@ -307,7 +188,9 @@ const Domicilio = () => {
                 <label htmlFor="floatingTextarea2">Referencia</label>
             </div>
             <h5>Fecha</h5>
-            <input type="date" className="form-control" placeholder="Fecha de retiro" aria-label="Fecha Retiro"></input>
->>>>>>> 90490544ac5aa540521ff623461fc3513ea06a94
+            <input type="date" className="form-control" placeholder="Fecha de retiro" aria-label="Fecha Retiro" onChange={handleFechaChange}></input>
+        </>
+)
+}
 
 export default Domicilio;
